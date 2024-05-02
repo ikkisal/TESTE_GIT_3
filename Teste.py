50f17f5d5130824417a77e2f9c28e4a0c997ab38
Ikey Salgado <Ikeysfr@gmail.com>
2024-05-02 11:01:09 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -1,6 +1,5 @@
 n1 = int(input('Digite um valor:' ))
 n2 = int(input('Digite outro valor:' ))
 s = n1+n2
-print(f'O valor de 45
-{n1} mais {n2} é igual a {s}')
+print(f'O valor de 45 {n1} mais {n2} é igual a {s}')
 
